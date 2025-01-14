1070041127c4d4fea6c4c149b138203827a86b17
JulyousDuarte2003 <98342044+Alexanderduarte2003@users.noreply.github.com>
2025-01-13 20:03:22 -0600

    first commit

---
+++ components/transition-component.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { fadeIn } from '@/utils/motion-transitions'
+import { motion } from 'framer-motion'
+
+interface MotionTransitionProps {
+    children: React.ReactNode
+    className?: string;
+    position: 'right' | 'bottom'
+}
+
+const MotionTransition = (props: MotionTransitionProps) => {
+    const { children, position, className } = props
+
+    return (
+        <motion.div
+        variants={fadeIn(position)}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        className={className}
+        >
+            {children}
+        </motion.div>
+    );
+}
+
+export default MotionTransition;
